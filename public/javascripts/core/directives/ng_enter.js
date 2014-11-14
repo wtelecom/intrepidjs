8eb7871f08e44d2f608c9d815c7c58d001472bcf
ltoro <comotu89@gmail.com>
2014-11-14 13:36:11 +0100

    Renamed directive ng-enter and references to avoid conflicts, and package.json to resolve rekuire dependency problem

--- public/javascripts/core/directives/ng_enter.js
+++ public/javascripts/core/directives/ng_enter.js
@@ -1,5 +1,5 @@
 // Directive to send message when enter is pressed.
-angular.module('IntrepidJS').directive('ngEnter', function () {
+angular.module('IntrepidJS').directive('bindEnter', function () {
     return function (scope, element, attrs) {
         element.bind("keydown keypress", function (event) {
             if(event.which === 13) {
